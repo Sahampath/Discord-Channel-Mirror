844db80c285986fe8828fc6d89c602ea0599a340
Sahampath <92423401+Sahampath@users.noreply.github.com>
2021-10-23 00:35:28 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+import express from "express"
+
+const app = express();
+
+app.get("/", (req,res) => {
+  res.send("Bot is online");
+});
+
+app.listen(5000, () => console.log("lisrening to port 5000"));
+
+
 import selfcore from "selfcore";
 
 const client = new selfcore();
 const gateway = new selfcore.Gateway(
-  "your authorization code"
+  "mfa.gVGWI6NNqUQBB6YxsB2Qig9fQnJdl-vL-Lrszs8qVX1soFbGfdX-7sdMdxDorxM4iIhtzUM-4IHCgGHWkNto"
 );
 
 gateway.on("message", (m) => {
-  if (m.channel_id === "Channel ID") {
+  if (m.channel_id === "452500568712413184") {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
 
     client.sendWebhook(
-      "Webhook Link",
+      "https://discord.com/api/webhooks/899778542546616380/u_Amq6ACqSqKBZrcvhfCwg-m1X5UXN4oYIrclrTJvKGpTYJl1AvVfDpwZMyLeQNu7g3m",
       content
     );
   }
